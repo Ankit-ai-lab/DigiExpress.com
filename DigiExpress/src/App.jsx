e9980b35230c3551c5796d000d3729696e164fa4
Ankit Gupta <134631639+Ankit-ai-lab@users.noreply.github.com>
2025-09-01 03:56:43 +0530

    index file done

--- DigiExpress/src/App.jsx
+++ DigiExpress/src/App.jsx
@@ -1,9 +1,26 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+
+
 
 function App() {
-
-
   return (
-  )}
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <Routes>
+      { <Route path="/" element={<Hero/>} /> }
+          {/* Add more routes later */}
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
+  )
+}
 
 export default App
